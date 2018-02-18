4d5128d11aa2339b6e3a9f78b5b8b37e50ae8b18
Ubuntu <ubuntu@ip-172-31-29-34.ec2.internal>
2018-02-18 19:49:07 +0000

    Successful deployment to Amazon EC2 instance with Nginx as a reverse proxy

--- start_dev_server.sh
+++ start_dev_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export LOCAL_IP_ADDRESS="127.0.0.7"
 export NODE_ENV="development"
-export PORT=7000
+export PORT=3000
 env npm start
--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export LOCAL_IP_ADDRESS="127.0.0.7"
 export PORT=3000
 export NODE_ENV="production"
 env npm start
