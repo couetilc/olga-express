91699f795ff51719748ebd1b9d89ead2507e8ffb
Connor Couetil <couetilc@gmail.com>
2018-02-18 16:02:47 -0500

    edits for local environment

--- start_dev_server.sh
+++ start_dev_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export LOCAL_IP_ADDRESS="localhost"
 export NODE_ENV="development"
 export PORT=7000
 env npm start
--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export LOCAL_IP_ADDRESS="localhost"
 export PORT=3000
 export NODE_ENV="production"
 env npm start
