8914b8d5e0462c46be88cd9d59add454aff02e46
Connor Couetil <couetilc@gmail.com>
2018-02-18 19:27:53 -0500

    Modified startup scripts to be managed by npm.

--- start_dev_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export LOCAL_IP_ADDRESS="localhost"
-export NODE_ENV="development"
-export PORT=3000
-env npm start
--- start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export LOCAL_IP_ADDRESS="localhost"
-export PORT=3000
-export NODE_ENV="production"
-env npm start
