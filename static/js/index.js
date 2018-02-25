343405d715e78113907cfa1f6244234ef42c96bc
Connor Couetil <couetilc@gmail.com>
2018-02-25 14:08:50 -0500

    Background successfully transitions on hover using Javascript. Simple style touchups related to background images

--- static/js/index.js
+++ static/js/index.js
@@ -21,13 +21,8 @@ function getImageCategory(img) {
     return match_category.exec(img.src)[1];
 }
 
-function toggleOpacity(element) {
-    console.log(element);
-    element.style.opacity = element.style.opacity ? 0 : 1;
-}
-
 function mapCategoryToBackgroundImg(imgs) {
-    let map = {};
+    let map = [];
     const match_category = /\/([a-z_]*)-[a-z\.]*$/i;
     imgs.forEach(img => {
         const category = match_category.exec(img.src)[1];
@@ -43,10 +38,25 @@ let cat2img = mapCategoryToBackgroundImg(
 
 thumbnails.forEach(thumb => {
     const category = getImageCategory(thumb.querySelector("img"));
-    thumb.addEventListener('mouseover', () => {
-        cat2img[category].classList.toggle('visible');
+
+    thumb.addEventListener('mouseenter', () => {
+        const categories = Object.keys(cat2img);
+        for (let c in categories) {
+            c = categories[c];
+            if (c === category) {
+                cat2img[c].classList.add('visible');
+            } else {
+                cat2img[c].classList.remove('visible');
+            }
+        }
     });
-    thumb.addEventListener('mouseleave', () => {
-        cat2img[category].classList.toggle('visible');
-    });
+});
+
+portfolio = document.querySelector("section.portfolio");
+portfolio.addEventListener('mouseleave', () => {
+    const categories = Object.keys(cat2img);
+    for (let c in categories) {
+        c = categories[c];
+        cat2img[c].classList.remove('visible');
+    }
 });
